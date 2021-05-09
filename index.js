bf6d7792efaa26aa019f93a93ecba8ae474fdc90
subhajitnag28 <subhajit.nag2805@gmail.com>
2021-05-09 13:47:28 +0530

    index modified

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require("express"),
-    // https = require('https'),
-    // fs = require('fs'),
     cors = require("cors"),
     app = express();
 
@@ -12,7 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static('public'));
 app.use('/resume', express.static(__dirname + '/uploads'));
-// app.set('port', 7000);
 
 for (const r in routes) {
     app.use("/" + r, routes[r]);
@@ -22,12 +19,3 @@ const port = process.env.PORT || 7000;
 app.listen(port, () => {
     console.log("Server is running on port :", port);
 });
-
-// const port = app.get('port');
-// const createServerOptions = {
-//     cert: fs.readFileSync('/etc/letsencrypt/archive/shoulderlihr.com/fullchain1.pem'),
-//     key: fs.readFileSync('/etc/letsencrypt/archive/shoulderlihr.com/privkey1.pem')
-// };
-// https.createServer(createServerOptions, app).listen(port, () => {
-//     console.log("Server is running on port :" + port)
-// });
