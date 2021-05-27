d7ce72782fc942148d543016cf12696ba49ef5fd
subhajitnag28 <subhajit.nag2805@gmail.com>
2021-05-27 13:23:31 +0530

    index js updated

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// const express = require("express"),
-//     cors = require("cors"),
-//     app = express();
+const express = require("express"),
+    cors = require("cors"),
+    app = express();
 
-// require("./dbConnection");
-// const routes = require("./routes/index");
+require("./dbConnection");
+const routes = require("./routes/index");
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
-// app.use(express.static('public'));
-// app.use('/resume', express.static(__dirname + '/uploads'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static('public'));
+app.use('/resume', express.static(__dirname + '/uploads'));
 
-// for (const r in routes) {
-//     app.use("/" + r, routes[r]);
-// }
+for (const r in routes) {
+    app.use("/" + r, routes[r]);
+}
 
-// const port = process.env.PORT || 7000;
-// app.listen(port, () => {
-//     console.log("Server is running on port :", port);
-// });
+const port = process.env.PORT || 7000;
+app.listen(port, () => {
+    console.log("Server is running on port :", port);
+});
